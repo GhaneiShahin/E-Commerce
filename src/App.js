f16b3c811d8a72ff055287eaef67dee504d296a5
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-06-15 20:07:22 +0200

    shop page, collections added!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router";
-import Homepage from "./containers/Homepage/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
+import Shop from "./pages/Shop/Shop";
 
 const App = () => {
   return (
     <Fragment>
       <Switch>
+      <Route path="/shop" component={Shop} />
         <Route exaxt path="/" component={Homepage} />
       </Switch>
     </Fragment>
---
+++ src/pages/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {shopData} from './shopData';
+import PreviewCollection from '../../components/PreviewCollection/PreviewCollection';
+
+export class Shop extends Component {
+  state = {
+    collections: shopData
+  }
+  render() {
+    const {collections} = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({id, ...otherCollectionsProps}) => (
+          <PreviewCollection id={id} {...otherCollectionsProps} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Shop
