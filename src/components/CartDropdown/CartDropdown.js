8803472f0bdedcea8b9dabb341b19d00204ca608
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-06-18 13:10:01 +0200

    created cart item component

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./CartDropdown.scss";
 import CostumButton from "../CostumButton/CostumButton";
+import CartItem from "../CartItem/CartItem";
+import { useSelector } from "react-redux";
 
 const CartDropdown = () => {
+  const cartItems = useSelector((state) => state.toggleCard.cartItems);
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CostumButton>GO TO CHECKOUT</CostumButton>
     </div>
   );
